61e425d91f0d505217a7e159c13e58392f97cf4b
lee <anomalocaris3871@gmail.com>
2022-04-12 20:48:10 +0900

    add srping pointcut study

---
+++ src/main/java/hello/aop/order/aop/AspectV6Advice.java
@@ -0,0 +1,48 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
+@Slf4j
+@Aspect
+public class AspectV6Advice {
+
+    /*    @Around("hello.aop.order.aop.Pointcuts.orderAndService()")
+        public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+            try {
+
+                log.info("[transaction start] {}", joinPoint.getSignature());
+                Object result = joinPoint.proceed();
+
+                log.info("[transaction commit] {}", joinPoint.getSignature());
+                return result;
+            } catch (Exception e) {
+                log.info("[transaction rollback] {}", joinPoint.getSignature());
+                throw e;
+            } finally {
+                log.info("[resource release] {}", joinPoint.getSignature());
+            }
+        }
+        */
+    @Before("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public void deBefore(JoinPoint joinPoint) {
+        log.info("[before] {}", joinPoint.getSignature());
+    }
+
+    @AfterReturning(value = "hello.aop.order.aop.Pointcuts.orderAndService()", returning = "result")
+    public void doReturn(JoinPoint joinPoint, Object result) {
+        log.info("[return] {} return={}", joinPoint.getSignature(), result);
+    }
+
+    @AfterThrowing(value = "hello.aop.order.aop.Pointcuts.orderAndService()", throwing = "ex")
+    public void doThrowing(JoinPoint joinPoint, Exception ex) {
+        log.info("[ex] {} message={}", joinPoint.getSignature(), ex);
+    }
+
+    @After(value = "hello.aop.order.aop.Pointcuts.orderAndService()")
+    public void doAfter(JoinPoint joinPoint) {
+        log.info("[after] {}", joinPoint.getSignature());
+    }
+}
